48439e1d3ad24e44078f6a536e2a8236cc578eec
Aurélie Violette <aureliev@theodo.fr>
2015-07-16 13:41:31 +0200

    Add mongo + remote method

--- common/models/todo-item.js
+++ common/models/todo-item.js
@@ -1,2 +1,15 @@
 module.exports = function(TodoItem) {
+  TodoItem.check = function(id, next) {
+    TodoItem.findById(id, {}, function(err, item){
+      if (err || !item) {next(err);}
+      item.updateAttributes({'doneDate': new Date(), 'isDone': true}, next);
+    });
+  };
+  TodoItem.remoteMethod('check',
+    {
+      http: {path: '/check', verb: 'post'},
+      accepts: [{arg:'id', type:'string', required: true}],
+      returns: {arg: 'item', type: 'object'}
+    }
+  );
 };
